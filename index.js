dcf3a0bdbc5c04ba5fc556de5a3b1a240baca446
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-04-25 22:30:41 +0600

    middleware added and dotenv file require

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express());
 
 app.get('/', (req,res)=>{
     res.send("jr-travelo server is running !");
