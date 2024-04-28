a8decc6dbecf8fe5419008652710b66953b3f993
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-04-28 12:45:13 +0600

    vercel json file added and uri change

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 
 
 // database connect
-const uri = "mongodb://localhost:27017";
+// const uri = "mongodb://localhost:27017";
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.k7qynmg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.k7qynmg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
